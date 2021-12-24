1e9dfcc80eeda5e69d54133ee6a23a0b45e29be7
Yusuf UZAN <yusuf.uzan@kariyer.net>
2021-12-24 17:07:20 +0300

    Search screen completed.

---
+++ Sources/MorePowerfulSwift/Extension/UIImageView+Load.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yusuf Uzan on 24.12.2021.
+//
+
+import UIKit
+
+public extension UIImageView {
+  func load(url: URL?,
+            placeholder: UIImage? = nil,
+            cache: URLCache? = nil) {
+    guard let url = url else { return }
+    let cache = cache ?? URLCache.shared
+    let request = URLRequest(url: url)
+    if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+      self.image = image
+    } else {
+      self.image = placeholder
+      URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+        if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
+          let cachedData = CachedURLResponse(response: response, data: data)
+          cache.storeCachedResponse(cachedData, for: request)
+          DispatchQueue.main.async {
+            self.image = image
+          }
+        }
+      }).resume()
+    }
+  }
+}
